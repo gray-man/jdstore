84996ba8f3aebd92a97477209ba7c0096ddcb0b7
liuhui <liuhuijisuanji@gmail.com>
2017-04-27 17:12:50 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  validates :billing_name
+  validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
