effdaf517cbf5a6f6b92c8abee06ae1a1575ae80
liuhui <liuhuijisuanji@gmail.com>
2017-04-27 10:29:49 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items
-  has_many :product, through: :cart_items, source: :product
+  has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
     ci = cart_items.build
@@ -11,12 +11,16 @@ class Cart < ApplicationRecord
 
   def total_price
     sum = 0
-    cart.cart_items.each do |cart_item|
+    cart_items.each do |cart_item|
       if cart_item.product.price.present?
         sum += cart_item.quantity * cart_item.product.price
       end
     end
     sum
+  end
+
+  def clean!
+    cart_items.destroy_all
   end
 
 end
