bd2c9c95b46033e3dd11511ae1583bd8f215d5df
liuhui <liuhuijisuanji@gmail.com>
2017-04-28 19:11:43 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,11 @@ Rails.application.routes.draw do
     resources :orders
   end
 
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
+
 end
